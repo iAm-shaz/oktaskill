b5f2dae77a81b26b0d3cf0120b4fff95da81ef84
shaaz <1992shaz@gmail.com>
2026-02-09 10:31:18 +0000

    auto-commit for bd374a04-c949-4262-9348-7eb8c933e3b3

--- frontend/src/components/animations/Animate.js
+++ frontend/src/components/animations/Animate.js
@@ -14,7 +14,7 @@ export function FadeIn({ children, className = '', delay = 0, direction = 'up' }
   return (
     <div
       ref={ref}
-      className={`transition-all duration-700 ease-out h-full ${
+      className={`transition-all duration-700 ease-out ${
         inView ? 'opacity-100 translate-x-0 translate-y-0' : `opacity-0 ${directionStyles[direction]}`
       } ${className}`}
       style={{ transitionDelay: `${delay}ms` }}
